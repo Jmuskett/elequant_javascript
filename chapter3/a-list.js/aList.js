96b03f9f62bffca29c803702a048e28203205b64
John Muskett <hi@johnmuskett.com>
2019-10-29 21:06:17 +0000

    add refactored list exercises

--- chapter3/a-list.js/aList.js
+++ chapter3/a-list.js/aList.js
@@ -1,6 +1,6 @@
 export const arrayToList = arr => {
   let list = null
-  let counter = arr.length - 1
+  let startPosition = arr.length - 1
 
   const inner = counter => {
     counter >= 0
@@ -8,7 +8,47 @@ export const arrayToList = arr => {
       : null
   }
 
-  inner(counter)
+  inner(startPosition)
 
   return list
+}
+
+export const listToArray = list => {
+  let arr = []
+
+  const inner = list => {
+    list.rest === null
+      ? arr.push(list.value)
+      : (arr.push(list.value), inner(list.rest))
+  }
+
+  inner(list)
+
+  return arr
+}
+
+export const prepend = (el, list) => {
+  let newList = {}
+
+  newList.value = el
+  newList.rest = list
+
+  return newList
+}
+
+export const nth = (index, list) => {
+  let counter = index
+  let val = undefined
+
+  const inner = (counter, list) => {
+    if (!list) {
+      val = undefined
+      return
+    }
+
+    counter === 0 ? (val = list.value) : inner(counter - 1, list.rest)
+  }
+
+  inner(counter, list)
+  return val
 }
--- chapter3/a-list.js/aList.test.js
+++ chapter3/a-list.js/aList.test.js
@@ -1,4 +1,4 @@
-import { arrayToList } from "./aList"
+import { arrayToList, listToArray, prepend, nth } from "./aList"
 
 //Write a function arrayToList that builds up
 //a list structure like the one
@@ -20,4 +20,78 @@ test("When given an array, the function returns a nested object", () => {
     }
   }
   expect(arrayToList([1, 2, 3])).toEqual(expected)
+})
+
+test("list to array is defined", () => {
+  expect(listToArray).toBeDefined()
+})
+
+test("when given a list, the function returns an array of its values", () => {
+  const expected = [1, 2, 3]
+  const list = {
+    value: 1,
+    rest: {
+      value: 2,
+      rest: {
+        value: 3,
+        rest: null
+      }
+    }
+  }
+  expect(listToArray(list)).toEqual(expected)
+})
+
+test("prepend is defined", () => {
+  expect(prepend).toBeDefined()
+})
+
+test("prepend takes an element and a list and creates a new list that adds the element to the front of the input list", () => {
+  const element = 5
+  const existingList = {
+    value: 1,
+    rest: {
+      value: 2,
+      rest: {
+        value: 3,
+        rest: null
+      }
+    }
+  }
+
+  const newList = {
+    value: 5,
+    rest: {
+      value: 1,
+      rest: {
+        value: 2,
+        rest: {
+          value: 3,
+          rest: null
+        }
+      }
+    }
+  }
+
+  expect(prepend(element, existingList)).toEqual(newList)
+})
+
+test("nth is defined", () => {
+  expect(nth).toBeDefined()
+})
+
+test("nth takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.", () => {
+  const list = {
+    value: 1,
+    rest: {
+      value: 2,
+      rest: {
+        value: 3,
+        rest: null
+      }
+    }
+  }
+
+  expect(nth(0, list)).toEqual(1)
+  expect(nth(2, list)).toEqual(3)
+  expect(nth(5, list)).toEqual(undefined)
 })
